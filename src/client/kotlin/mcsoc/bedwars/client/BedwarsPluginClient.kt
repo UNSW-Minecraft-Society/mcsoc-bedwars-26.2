22eb0798f6544d317bbcaacfec84bd86a3fd52e4
veveddo <68318644+Vedvod@users.noreply.github.com>
2026-07-21 03:54:12 +1000

    initial commit

---
+++ src/client/kotlin/mcsoc/bedwars/client/BedwarsPluginClient.kt
@@ -0,0 +1,9 @@
+package mcsoc.bedwars.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object BedwarsPluginClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/mcsoc/bedwars/client/BedwarsPluginDataGenerator.kt
@@ -0,0 +1,9 @@
+package mcsoc.bedwars.client
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object BedwarsPluginDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
