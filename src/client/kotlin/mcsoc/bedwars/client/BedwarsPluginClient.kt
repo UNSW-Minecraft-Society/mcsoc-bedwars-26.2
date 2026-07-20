f77c1e46a58b66763f6a178b61ce0b22cc0d1c72
veveddo <68318644+Vedvod@users.noreply.github.com>
2026-07-21 04:06:43 +1000

    remove client code src and entrypoint

--- src/client/kotlin/mcsoc/bedwars/client/BedwarsPluginClient.kt
+++
@@ -1,9 +0,0 @@
-package mcsoc.bedwars.client
-
-import net.fabricmc.api.ClientModInitializer
-
-object BedwarsPluginClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
--- src/client/kotlin/mcsoc/bedwars/client/BedwarsPluginDataGenerator.kt
+++
@@ -1,9 +0,0 @@
-package mcsoc.bedwars.client
-
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
-
-object BedwarsPluginDataGenerator : DataGeneratorEntrypoint {
-	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
-	}
-}
