a3c594174c3cd161af4a87ac8a016618bed94b29
Quack2Alice <quackquackalice.120@gmail.com>
2026-08-18 16:08:23 +1000

    Merge personal-upgrades into branch.

--- src/main/kotlin/mcsoc/bedwars/eventhandlers/CommandRegistration.kt
+++ src/main/kotlin/mcsoc/bedwars/eventhandlers/CommandRegistration.kt
@@ -16,44 +16,42 @@ const val ROOT_NODE = "bedwars"
 fun registerCommands() {
     CommandRegistrationCallback.EVENT.register{dispatcher, buildContext, selection ->
         dispatcher.register(Commands.literal(ROOT_NODE)
-        .then(Commands.literal("open_shop_gui").executes(ShopGui::displayShop))
+        .requires { source -> source.permissions().hasPermission(Permissions.COMMANDS_MODERATOR) }
+            .then(Commands.literal("open_shop_gui").executes(ShopGui::displayShop))
             .then(Commands.literal("test_simple_gui").executes(ShopGui::testSimpleGui))
+
             .then(Commands.literal("test_simple_gui_4").executes(ShopGui::testSimpleGui4))
-    CommandRegistrationCallback.EVENT.register { dispatcher, buildContext, selection ->
-        dispatcher.register(
-            Commands.literal(ROOT_NODE)
-                .requires { source -> source.permissions().hasPermission(Permissions.COMMANDS_MODERATOR) }
 
-                // only test commands
-                // use: /bedwars upgradetool [Axe|Pickaxe|...]
-                .then(
-                    Commands.literal("upgrade").then(
-                        Commands.argument("type", StringArgumentType.word())
-                            .suggests(UpgradeItemsSuggestionProvider)
-                            .executes {
-                                val player = it.source.player ?: run {
-                                    it.source.sendFailure(Component.literal("Command must be run by a player"))
-                                    return@executes 0
-                                }
-                                val input = StringArgumentType.getString(it, "type")
-                                val type = UpgradeItemType.entries.firstOrNull { name ->
-                                    name.name.equals(input, ignoreCase = true)
-                                } ?: run {
-                                    player.sendSystemMessage(Component.literal("$input is not a valid upgrade"))
-                                    return@executes 0
-                                }
-
-                                ModDataTracker.upgradeItem(player, type)
-                                1
+            // only test commands
+            // use: /bedwars upgradetool [Axe|Pickaxe|...]
+            .then(
+                Commands.literal("upgrade").then(
+                    Commands.argument("type", StringArgumentType.word())
+                        .suggests(UpgradeItemsSuggestionProvider)
+                        .executes {
+                            val player = it.source.player ?: run {
+                                it.source.sendFailure(Component.literal("Command must be run by a player"))
+                                return@executes 0
                             }
-                    )
-                )
+                            val input = StringArgumentType.getString(it, "type")
+                            val type = UpgradeItemType.entries.firstOrNull { name ->
+                                name.name.equals(input, ignoreCase = true)
+                            } ?: run {
+                                player.sendSystemMessage(Component.literal("$input is not a valid upgrade"))
+                                return@executes 0
+                            }
 
-                .then(Commands.literal("resettools").executes {
-                    val player = it.source.playerOrException
-                    ModDataTracker.clearItems(player)
-                    1
-                })
+                            ModDataTracker.upgradeItem(player, type)
+                            1
+                        }
+                )
+            )
+
+            .then(Commands.literal("resettools").executes {
+                val player = it.source.playerOrException
+                ModDataTracker.clearItems(player)
+                1
+            })
         )
     }
 }
