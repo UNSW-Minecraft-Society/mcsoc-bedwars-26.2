4df428104eb3e2c91c35d80f79149062ce8eef32
veveddo <68318644+Vedvod@users.noreply.github.com>
2026-08-22 13:37:08 +1000

    put CylindricalBlockPos data values on different lines

--- src/main/kotlin/mcsoc/bedwars/utils/MathUtils.kt
+++ src/main/kotlin/mcsoc/bedwars/utils/MathUtils.kt
@@ -1,13 +1,19 @@
 package mcsoc.bedwars.utils
 
-import net.minecraft.core.BlockPos
 import kotlin.math.atan2
 import kotlin.math.cos
 import kotlin.math.hypot
 import kotlin.math.roundToInt
 import kotlin.math.sin
+import net.minecraft.core.BlockPos
 
-data class CylindricalBlockPos(val origin: BlockPos, var radius: Float, var angle: Float, var height: Int) {
+
+data class CylindricalBlockPos(
+    var origin: BlockPos = BlockPos(0, 0, 0),
+    var radius: Float = 0F,
+    var angle: Float = 0F,
+    var height: Int = 0
+) {
     companion object {
         fun BlockPos.toCylindricalBlockPos(): CylindricalBlockPos {
             val new_angle = atan2(this.x.toFloat(), this.z.toFloat())
