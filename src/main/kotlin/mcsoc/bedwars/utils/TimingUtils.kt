494b3bc18e0dcf9064876e0d22ef68bbd2316fce
veveddo <68318644+Vedvod@users.noreply.github.com>
2026-08-27 02:32:58 +1000

    fix ticks extension being literally wrong

--- src/main/kotlin/mcsoc/bedwars/utils/TimingUtils.kt
+++ src/main/kotlin/mcsoc/bedwars/utils/TimingUtils.kt
@@ -1,12 +1,13 @@
 package mcsoc.bedwars.utils
 
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
 
 inline val Int.ticks: Duration 
-    get() = toDuration(DurationUnit.SECONDS) * 20
+    get() = this.seconds / 20
 
-inline val Duration.inWholeTicks: Duration
+inline val Duration.inWholeTicks: Duration 
     get() = ((inWholeMilliseconds / 50).toInt() * 50).toDuration(DurationUnit.MILLISECONDS)
