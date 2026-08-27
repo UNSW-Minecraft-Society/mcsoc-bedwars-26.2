bb42b55c72a4471b4938180075dae26061910408
Quack2Alice <quackquackalice.120@gmail.com>
2026-08-27 22:19:37 +1000

    Fix fireball functionality

--- src/main/kotlin/mcsoc/bedwars/eventhandlers/InteractionRegistration.kt
+++ src/main/kotlin/mcsoc/bedwars/eventhandlers/InteractionRegistration.kt
@@ -4,6 +4,7 @@ import mcsoc.bedwars.BedwarsPlugin
 import mcsoc.bedwars.items.CUSTOM_ITEM_TAG
 import mcsoc.bedwars.items.CustomItemInteraction
 import mcsoc.bedwars.datatrackers.ModDataTracker
+import net.fabricmc.fabric.api.event.player.UseBlockCallback
 import net.fabricmc.fabric.api.event.player.UseItemCallback
 import net.minecraft.core.component.DataComponents
 import net.minecraft.world.InteractionResult
@@ -14,19 +15,12 @@ import net.minecraft.world.InteractionResult
 fun registerItemCallbacks() {
     // Alive UseItemCallbacks
     UseItemCallback.EVENT.register {player, level, hand ->
-        if (ModDataTracker.isPlayerAlive(player)) {
-            BedwarsPlugin.LOGGER.info("Player is alive")
-            val item = player.getItemInHand(hand)
-            if (item.get(DataComponents.CUSTOM_DATA)?.copyTag()?.contains(CUSTOM_ITEM_TAG) ?: false) {
-                val value = item.get(DataComponents.CUSTOM_DATA)
-                    ?.copyTag()?.getString(CUSTOM_ITEM_TAG)?.get()
-                BedwarsPlugin.LOGGER.info("Item has $CUSTOM_ITEM_TAG $value")
-                CustomItemInteraction.triggerCustomItemEffect(player, level, item, value)
-            } else {
-                return@register InteractionResult.FAIL
-            }
-        } else {
-            return@register InteractionResult.PASS
-        }
+        BedwarsPlugin.LOGGER.info("UseItem called")
+        return@register CustomItemInteraction.triggerCustomItemEffect(player, level, hand)
+    }
+    UseBlockCallback.EVENT.register { player, level, hand, hitResult ->
+        BedwarsPlugin.LOGGER.info("UseBlock called")
+        return@register CustomItemInteraction.triggerCustomItemEffect(player, level, hand)
     }
 }
+
--- src/main/kotlin/mcsoc/bedwars/items/CustomItemInteraction.kt
+++ src/main/kotlin/mcsoc/bedwars/items/CustomItemInteraction.kt
@@ -1,20 +1,37 @@
 package mcsoc.bedwars.items
 
 import mcsoc.bedwars.BedwarsPlugin
+import mcsoc.bedwars.datatrackers.ModDataTracker
+import net.minecraft.core.component.DataComponents
+import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.EntityTypes
+import net.minecraft.world.entity.LightningBolt
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.entity.projectile.hurtingprojectile.LargeFireball
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
+import net.minecraft.world.phys.HitResult
+import net.minecraft.world.phys.Vec3
 
 const val CUSTOM_FIREBALL_VAL = "fireball"
 const val CUSTOM_BRIDGE_EGG_VAL = "bridge_egg"
 const val CUSTOM_POPUP_TOWER_VAL = "popup_tower"
 const val CUSTOM_PLAYER_TRACKER_VAL = "player_tracker"
 
+const val FIREBALL_SPEED = 1.0
+
 object CustomItemInteraction {
-    fun triggerCustomItemEffect(player: Player, level: Level, item: ItemStack, type: String?): InteractionResult {
+    fun triggerCustomItemEffect(player: Player, level: Level, hand: InteractionHand, hitResult: HitResult? = null): InteractionResult {
+        val item = player.getItemInHand(hand)
+        if (!ModDataTracker.isPlayerAlive(player))
+            return InteractionResult.PASS
+        if (!(item.get(DataComponents.CUSTOM_DATA)?.copyTag()?.contains(CUSTOM_ITEM_TAG) ?: false))
+            return InteractionResult.PASS
+        if (level.isClientSide)
+            return InteractionResult.PASS
+        val type = item.get(DataComponents.CUSTOM_DATA)?.copyTag()?.getString(CUSTOM_ITEM_TAG)?.get()
+        BedwarsPlugin.LOGGER.info("Item has $CUSTOM_ITEM_TAG $type")
         when (type) {
             CUSTOM_FIREBALL_VAL -> useFireballEffect(player, level, item)
             CUSTOM_BRIDGE_EGG_VAL -> {}
@@ -26,12 +43,13 @@ object CustomItemInteraction {
 
     fun useFireballEffect(player: Player, level: Level, item: ItemStack): InteractionResult {
         BedwarsPlugin.LOGGER.info("Doing fireball thing")
-        val direction = player.direction
+        val directionVector = player.getViewVector(1.0f)
         var fireball = LargeFireball(EntityTypes.FIREBALL, level)
-        fireball.setPos(player.position().relative(direction, 0.5))
+        fireball.setPos(player.eyePosition.add(directionVector.scale(0.5)))
         fireball.owner = player
-        fireball.deltaMovement = direction.unitVec3
+        fireball.deltaMovement = directionVector.scale(FIREBALL_SPEED)
         level.addFreshEntity(fireball)
+        if (!player.isCreative) item.count -= 1
         return InteractionResult.SUCCESS
     }
 }
