8d585bc2b525ba367a35b10e9cfef58a07b4b68a
Ian <iantiancen@gmail.com>
2026-08-30 20:04:19 +1000

    used objects instead of companion objects

--- src/main/kotlin/mcsoc/bedwars/eventhandlers/EventRegistration.kt
+++ src/main/kotlin/mcsoc/bedwars/eventhandlers/EventRegistration.kt
@@ -3,14 +3,12 @@ package mcsoc.bedwars.eventhandlers
 import mcsoc.bedwars.datatrackers.ModDataTracker
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
 
-class EventRegistration {
-    companion object {
-        fun registerEventHandlers() {
-            ServerTickEvents.END_SERVER_TICK.register{server ->
-                ModDataTracker.tick()
-            }
-            registerItemCallbacks()
-            registerPlayerJoinEvent()
+object EventRegistration {
+    fun registerEventHandlers() {
+        ServerTickEvents.END_SERVER_TICK.register{server ->
+            ModDataTracker.tick()
         }
+        registerItemCallbacks()
+        registerPlayerJoinEvent()
     }
 }
--- src/main/kotlin/mcsoc/bedwars/eventhandlers/commands/CommandRegistration.kt
+++ src/main/kotlin/mcsoc/bedwars/eventhandlers/commands/CommandRegistration.kt
@@ -12,40 +12,38 @@ const val ROOT_NODE = "bedwars"
 
 const val SOME_ARGUMENT = "some"
 
-class CommandRegistration {
-    companion object {
-        /**
-         * Function to register commands for the plugin
-         */
-        fun registerCommands() {
-            CommandRegistrationCallback.EVENT.register { dispatcher, buildContext, selection ->
-                dispatcher.register(
-                    Commands.literal(ROOT_NODE)
-                        .then(
-                            Commands.literal("ping")
-                                .requires { it.permissions().hasPermission(Permissions.COMMANDS_GAMEMASTER) }
-                                .executes(CommandActions::ping)
-                                .then(
-                                    Commands.argument(SOME_ARGUMENT, StringArgumentType.word())
-                                        .suggests(ExampleSuggestionProvider())
-                                        .executes(CommandActions::pingWord)
-                                )
-                        )
-                        .then(Commands.literal("join").executes(CommandActions::join))
-                        .then(Commands.literal("leave").executes(CommandActions::leave))
-                        .then(Commands.literal("get_team").executes(CommandActions::getTeam))
-                        .then(
-                            Commands.literal("assign_teams")
-                                .requires { source ->
-                                    source.permissions().hasPermission(Permissions.COMMANDS_MODERATOR)
-                                }
-                                .then(
-                                    Commands.argument("number_of_teams", IntegerArgumentType.integer())
-                                        .executes(CommandActions::assignTeams)
-                                )
-                        )
-                )
-            }
+object CommandRegistration {
+    /**
+     * Function to register commands for the plugin
+     */
+    fun registerCommands() {
+        CommandRegistrationCallback.EVENT.register { dispatcher, buildContext, selection ->
+            dispatcher.register(
+                Commands.literal(ROOT_NODE)
+                    .then(
+                        Commands.literal("ping")
+                            .requires { it.permissions().hasPermission(Permissions.COMMANDS_GAMEMASTER) }
+                            .executes(CommandActions::ping)
+                            .then(
+                                Commands.argument(SOME_ARGUMENT, StringArgumentType.word())
+                                    .suggests(ExampleSuggestionProvider())
+                                    .executes(CommandActions::pingWord)
+                            )
+                    )
+                    .then(Commands.literal("join").executes(CommandActions::join))
+                    .then(Commands.literal("leave").executes(CommandActions::leave))
+                    .then(Commands.literal("get_team").executes(CommandActions::getTeam))
+                    .then(
+                        Commands.literal("assign_teams")
+                            .requires { source ->
+                                source.permissions().hasPermission(Permissions.COMMANDS_MODERATOR)
+                            }
+                            .then(
+                                Commands.argument("number_of_teams", IntegerArgumentType.integer())
+                                    .executes(CommandActions::assignTeams)
+                            )
+                    )
+            )
         }
     }
 }
