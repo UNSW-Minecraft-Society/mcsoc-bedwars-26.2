a25a100a9bcaabc30a367f5d4d624cb429cff8f4
gbouf_06 <boufaycalg@gmail.com>
2026-08-31 14:18:19 +1000

    Update DisableCrafingMixin.java

--- src/main/java/mcsoc/bedwars/mixin/DisableCraftingMixin.java
+++ src/main/java/mcsoc/bedwars/mixin/DisableCraftingMixin.java
@@ -1,11 +1,11 @@
 package mcsoc.bedwars.mixin;
 
-import net.minecraft.recipe.RecipeInput;
-import net.minecraft.recipe.RecipeManager;
-import net.minecraft.recipe.Recipe;
-import net.minecraft.recipe.RecipeEntry;
-import net.minecraft.recipe.RecipeType;
-import net.minecraft.world.World;
+import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraft.world.item.crafting.RecipeInput;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,8 +14,9 @@ import java.util.Optional;
 
 @Mixin(RecipeManager.class)
 public class DisableCraftingMixin {
-    @Inject(at = @At("HEAD"), method = "getFirstMatch", cancellable = true)
-    private <I extends RecipeInput, T extends Recipe<I>> void blockCrafting(RecipeType<T> type, I input, World world, CallbackInfoReturnable<Optional<RecipeEntry<T>>> cir) {
+    @Inject(at = @At("HEAD"), method = "getRecipeFor", cancellable = true)
+    private <I extends RecipeInput, T extends Recipe<I>> void blockCrafting(
+        RecipeType<T> type, I input, Level level, CallbackInfoReturnable<Optional<RecipeHolder<T>>> cir) {
             if (type == RecipeType.CRAFTING) {
                 cir.setReturnValue(Optional.empty());
             }
