20cde22d58e2e0978c827a25d25ac4fdb90ec6fd
veveddo <68318644+Vedvod@users.noreply.github.com>
2026-09-06 18:16:10 +1000

    register switch command and also tweak other commands

--- src/main/kotlin/mcsoc/bedwars/eventhandlers/commands/CommandRegistration.kt
+++ src/main/kotlin/mcsoc/bedwars/eventhandlers/commands/CommandRegistration.kt
@@ -1,9 +1,11 @@
 package mcsoc.bedwars.eventhandlers.commands
 
 
+import com.mojang.brigadier.arguments.BoolArgumentType
 import com.mojang.brigadier.arguments.IntegerArgumentType
 import com.mojang.brigadier.arguments.StringArgumentType
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
+import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.Commands
 import net.minecraft.commands.arguments.coordinates.BlockPosArgument
 import net.minecraft.server.permissions.Permissions
@@ -12,10 +14,12 @@ import net.minecraft.server.permissions.Permissions
 const val ROOT_NODE = "bedwars"
 
 const val SOME_ARGUMENT = "some"
+const val BOOL_ARGUMENT = "bool"
 const val UPGRADE_TYPE_ARG = "type"
 const val FIRST_POSITION_ARGUMENT = "pos1"
 const val SECOND_POSITION_ARGUMENT = "pos2"
 
+val GAMEMASTER_PERMS_REQUIREMENT: (CommandSourceStack) -> Boolean = { source -> source.permissions().hasPermission(Permissions.COMMANDS_MODERATOR)}
 
 /**
  * Function to register commands for the plugin
@@ -26,7 +30,7 @@ fun registerCommands() {
         Commands.literal(ROOT_NODE)
             .then(
                 Commands.literal("ping")
-                    .requires { it.permissions().hasPermission(Permissions.COMMANDS_GAMEMASTER) }
+                    .requires(GAMEMASTER_PERMS_REQUIREMENT)
                     .executes(CommandActions::ping)
                     .then(
                         Commands.argument(SOME_ARGUMENT, StringArgumentType.word())
@@ -34,37 +38,40 @@ fun registerCommands() {
                             .executes(CommandActions::pingWord)
                     )
                     .then(Commands.literal("start")
-                        .requires { source -> source.permissions().hasPermission(Permissions.COMMANDS_MODERATOR)}
+                        .requires(GAMEMASTER_PERMS_REQUIREMENT)
                         .executes(CommandActions::start))
                     .then(Commands.literal("end")
-                        .requires { source -> source.permissions().hasPermission(Permissions.COMMANDS_MODERATOR)}
+                        .requires(GAMEMASTER_PERMS_REQUIREMENT)
                         .executes(CommandActions::end))
             )
-            .then(Commands.literal("join").executes(CommandActions::join))
-            .then(Commands.literal("leave").executes(CommandActions::leave))
-            .then(Commands.literal("get_team").executes(CommandActions::getTeam))
-            .then(
-                Commands.literal("assign_teams")
-                    .requires { source ->
-                        source.permissions().hasPermission(Permissions.COMMANDS_MODERATOR)
-                    }
-                    .then(
-                        Commands.argument("number_of_teams", IntegerArgumentType.integer())
-                            .executes(CommandActions::assignTeams)
-                    )
+            .then(Commands.literal("join")
+            .executes(CommandActions::join)
+            )
+            .then(Commands.literal("leave")
+            .executes(CommandActions::leave)
+            )
+            .then(Commands.literal("get_team")
+            .executes(CommandActions::getTeam)
+            )
+            .then(Commands.literal("assign_teams")
+            .requires(GAMEMASTER_PERMS_REQUIREMENT)
+                .then(Commands.argument("number_of_teams", IntegerArgumentType.integer())
+                .executes(CommandActions::assignTeams)
+                )
             )
             .then(Commands.literal("upgrade")
-                .requires { source -> source.permissions().hasPermission(Permissions.COMMANDS_MODERATOR)}
+            .requires(GAMEMASTER_PERMS_REQUIREMENT)
                 .then(Commands.argument(UPGRADE_TYPE_ARG, StringArgumentType.word())
                     .suggests(UpgradeItemsSuggestionProvider())
                     .executes(CommandActions::upgradeItem)
                 )
             )
             .then(Commands.literal("reset_upgrades")
-                .requires { source -> source.permissions().hasPermission(Permissions.COMMANDS_MODERATOR)}
-                .executes(CommandActions::resetUpgrades)
+            .requires(GAMEMASTER_PERMS_REQUIREMENT)
+            .executes(CommandActions::resetUpgrades)
             )
             .then(Commands.literal("set_protection_zone")
+                .requires(GAMEMASTER_PERMS_REQUIREMENT)
                 .then(Commands.argument(FIRST_POSITION_ARGUMENT, BlockPosArgument.blockPos())
                     .then(Commands.argument(SECOND_POSITION_ARGUMENT, BlockPosArgument.blockPos())
                     .executes(CommandActions::setProtectionZone)
@@ -73,6 +80,15 @@ fun registerCommands() {
             )
             .then(Commands.literal("list_protection_zones")
             .executes(CommandActions::listProtectionZones)
+            )
+            .then(Commands.literal("protection_state")
+                .executes(CommandActions::getProtectionState)
+                .then(Commands.literal("set")
+                .requires(GAMEMASTER_PERMS_REQUIREMENT)
+                    .then(Commands.argument(BOOL_ARGUMENT, BoolArgumentType.bool())
+                    .executes(CommandActions::setProtectionState)
+                    )
+                )
             )
         )
     }
