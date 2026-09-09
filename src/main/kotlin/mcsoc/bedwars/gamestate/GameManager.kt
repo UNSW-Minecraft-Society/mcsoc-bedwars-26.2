f3d583c44cfa136e07a7810581f30bc32707e453
Ian <iantiancen@gmail.com>
2026-09-09 18:53:28 +1000

    Clear inventory + downgrade items on death

--- src/main/kotlin/mcsoc/bedwars/gamestate/GameManager.kt
+++ src/main/kotlin/mcsoc/bedwars/gamestate/GameManager.kt
@@ -147,6 +147,10 @@ class GameManager {
 //                }
 //            }
 
+
+            player.inventory.clearContent()
+            level_mod_data.downgradeItems(player)
+
             // store player's death position to summon lightning later. Due to the nature of this event handler,
             // all players are forced to enter "DEAD" state upon death.
             level_mod_data.setPlayerDead(player, player.position())
