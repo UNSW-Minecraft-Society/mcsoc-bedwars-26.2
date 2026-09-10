3aa45496e9007c7bd69684abb2c103dc4be83e80
Vedvod <68318644+Vedvod@users.noreply.github.com>
2026-09-10 22:53:54 +1000

    Update build.gradle.kts
    include ktoml

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,9 +57,13 @@ dependencies {
 	
 	// toml and yaml support
 	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
+    include("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
 	implementation("com.akuleshov7:ktoml-core:0.7.1")
+    include("com.akuleshov7:ktoml-core:0.7.1")
 	implementation("com.akuleshov7:ktoml-file:0.7.1")
+    include("com.akuleshov7:ktoml-file:0.7.1")
 	implementation("io.heapy.kotaml:kotaml:0.110.0")
+    include("io.heapy.kotaml:kotaml:0.110.0")
 
 	implementation("eu.pb4:sgui:2.1.0+26.2")
 	include("eu.pb4:sgui:2.1.0+26.2")
