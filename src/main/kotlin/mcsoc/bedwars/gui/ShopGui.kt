01cebf72cca359c8dd971ad846eaa3de044b95bf
Quack2Alice <quackquackalice.120@gmail.com>
2026-09-12 18:20:10 +1000

    Update Shop Products

--- src/main/kotlin/mcsoc/bedwars/gui/ShopGui.kt
+++ src/main/kotlin/mcsoc/bedwars/gui/ShopGui.kt
@@ -50,23 +50,23 @@ object ShopGui {
              ShopType.PLAYER_SHOP to arrayOf(
                  // These are ShopPlayerUpgrades
                  ShopPlayerUpgrade(UpgradeItemType.ARMOUR,
-                     arrayOf(Items.GOLD_INGOT, Items.DIAMOND, Items.EMERALD),
-                     arrayOf(3, 4, 5),
+                     arrayOf(Items.IRON_INGOT, Items.GOLD_INGOT, Items.EMERALD),
+                     arrayOf(20, 12, 6),
                      arrayOf("Chainmail Armor", "Iron Armor", "Diamond Armor")
                  ),
                  ShopPlayerUpgrade(UpgradeItemType.SWORD,
-                     arrayOf(Items.IRON_INGOT, Items.GOLD_INGOT, Items.DIAMOND),
-                     arrayOf(3, 4, 5),
+                     arrayOf(Items.IRON_INGOT, Items.GOLD_INGOT, Items.EMERALD),
+                     arrayOf(10, 7, 3),
                      arrayOf("Stone Sword", "Iron Sword", "Diamond Sword")
                  ),
                  ShopPlayerUpgrade(UpgradeItemType.PICKAXE,
-                     arrayOf(Items.IRON_INGOT, Items.GOLD_INGOT, Items.DIAMOND, Items.EMERALD),
-                     arrayOf(1, 2, 3, 4),
+                     arrayOf(Items.IRON_INGOT, Items.IRON_INGOT, Items.GOLD_INGOT, Items.GOLD_INGOT),
+                     arrayOf(5, 10, 3, 6),
                      arrayOf("Wooden Pickaxe", "Iron Pickaxe", "Golden Pickaxe", "Diamond Pickaxe")
                  ),
                  ShopPlayerUpgrade(UpgradeItemType.AXE,
-                     arrayOf(Items.IRON_INGOT, Items.GOLD_INGOT, Items.DIAMOND, Items.EMERALD),
-                     arrayOf(1, 2, 3, 4),
+                     arrayOf(Items.IRON_INGOT, Items.IRON_INGOT, Items.GOLD_INGOT, Items.GOLD_INGOT),
+                     arrayOf(5, 10, 3, 6),
                      arrayOf("Wooden Axe", "Stone Axe", "Iron Axe", "Diamond Axe")
                  ),
                  ShopPlayerUpgrade(UpgradeItemType.SHEARS,
@@ -80,35 +80,37 @@ object ShopGui {
                  ShopPlayerCustomItem({player -> BedwarsItems.knockbackStickItemStack(player.level())}, Items.GOLD_INGOT, 5),
                  ShopItem(Items.WATER_BUCKET, 1, Items.GOLD_INGOT, 6),
 
-                 ShopItem(Items.ARROW, 16, Items.GOLD_INGOT, 2),
-                 EmptyShopProduct(),
-                 EmptyShopProduct(),
-                 EmptyShopProduct(),
-                 EmptyShopProduct(),
-
-                 ShopTeamItem(Team.entries.associateWith { Items.WOOL.pick(it.dyeColour) },
-                     16, Items.IRON_INGOT, 4),
-                 ShopItem(Items.SANDSTONE, 16, Items.IRON_INGOT, 16),
-                 ShopItem(Items.END_STONE, 12, Items.IRON_INGOT, 24),
-                 ShopItem(Items.OBSIDIAN, 4, Items.EMERALD, 4),
-                 ShopItem(Items.OAK_PLANKS, 16, Items.GOLD_INGOT, 6),
-
-                 ShopItem(Items.LADDER, 16, Items.IRON_INGOT, 4),
-                 ShopItem(Items.WIND_CHARGE, 1, Items.GOLD_INGOT, 24),
-                 ShopCustomItem(BedwarsItems::popupTowerItemStack, Items.GOLD_INGOT, 24),
-                 ShopCustomItem(BedwarsItems::bridgeEggItemStack, Items.EMERALD, 1),
-                 ShopItem(Items.ENDER_PEARL, 1, Items.EMERALD, 4),
-
-                 ShopCustomItem(BedwarsItems::ballOfBugsItemStack, Items.GOLD_INGOT, 2),
-                 ShopCustomItem(BedwarsItems::fireballItemStack, Items.IRON_INGOT, 36),
-                 ShopCustomItem(BedwarsItems::instantTNTItemStack, Items.GOLD_INGOT, 8),
-                 ShopItem(Items.IRON_GOLEM_SPAWN_EGG, 2, Items.IRON_INGOT, 120),
-                 EmptyShopProduct(),
-
+                 ShopItem(Items.ARROW, 8, Items.GOLD_INGOT, 2),
                  ShopItem(Items.GOLDEN_APPLE, 1, Items.GOLD_INGOT, 3),
                  ShopCustomItem({BedwarsItems.potionItemStack(Potions.LEAPING)}, Items.EMERALD, 1),
                  ShopCustomItem({BedwarsItems.potionItemStack(Potions.SWIFTNESS)}, Items.EMERALD, 1),
                  ShopCustomItem({BedwarsItems.potionItemStack(Potions.INVISIBILITY)}, Items.EMERALD, 2),
+
+                 ShopTeamItem(Team.entries.associateWith { Items.WOOL.pick(it.dyeColour) },
+                     16, Items.IRON_INGOT, 4),
+                 ShopItem(Items.SANDSTONE, 16, Items.IRON_INGOT, 16),
+                 ShopItem(Items.END_STONE, 16, Items.IRON_INGOT, 24),
+                 ShopItem(Items.OBSIDIAN, 4, Items.EMERALD, 4),
+                 ShopItem(Items.OAK_PLANKS, 16, Items.GOLD_INGOT, 6),
+
+                 ShopTeamItem(Team.entries.associateWith { Items.STAINED_GLASS.pick(it.dyeColour) },
+                     4, Items.IRON_INGOT, 11),
+                 ShopItem(Items.CLAY, 16, Items.IRON_INGOT, 12),
+                 ShopItem(Items.LADDER, 16, Items.IRON_INGOT, 4),
+                 EmptyShopProduct(),
+                 EmptyShopProduct(),
+
+
+                 ShopItem(Items.WIND_CHARGE, 1, Items.GOLD_INGOT, 24),
+                 ShopCustomItem(BedwarsItems::popupTowerItemStack, Items.GOLD_INGOT, 24),
+                 ShopCustomItem(BedwarsItems::bridgeEggItemStack, Items.EMERALD, 1),
+                 ShopItem(Items.ENDER_PEARL, 1, Items.EMERALD, 4),
+                 EmptyShopProduct(),
+
+                 ShopCustomItem(BedwarsItems::ballOfBugsItemStack, Items.GOLD_INGOT, 2),
+                 ShopCustomItem(BedwarsItems::fireballItemStack, Items.IRON_INGOT, 36),
+                 ShopCustomItem(BedwarsItems::instantTNTItemStack, Items.GOLD_INGOT, 8),
+                 EmptyShopProduct(),
                  EmptyShopProduct(),
              ),
              ShopType.TEAM_SHOP to arrayOf(
