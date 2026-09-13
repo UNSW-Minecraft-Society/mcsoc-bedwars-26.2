1a066b7ba996b18916af7f1e7c72a93489df0804
Quack2Alice <quackquackalice.120@gmail.com>
2026-09-13 18:18:44 +1000

    Edit GameEvent Methods n Stuff

--- src/main/kotlin/mcsoc/bedwars/datatrackers/GameEventTracker.kt
+++ src/main/kotlin/mcsoc/bedwars/datatrackers/GameEventTracker.kt
@@ -4,35 +4,29 @@ import mcsoc.bedwars.gamestate.GameEvent
 import net.minecraft.server.level.ServerLevel
 import java.util.PriorityQueue
 
-data class ScheduledGameEvent(val gameEvent: GameEvent, val tickNumber: Int) : Comparable<ScheduledGameEvent> {
-    override fun compareTo(other: ScheduledGameEvent): Int {
-        return tickNumber.compareTo(other.tickNumber)
-    }
-}
-
 sealed class GameEventTracker {
     val level: ServerLevel
-    private var tickNumber: Int = 0
-    val eventQueue = PriorityQueue<ScheduledGameEvent>()
+    private var currTickNumber: Int = 0
+    val eventQueue = PriorityQueue<GameEvent>()
 
     protected constructor(level: ServerLevel) {
         this.level = level
     }
 
-    fun queueEvent(event: GameEvent, numTicks: Int) {
-        eventQueue.add(ScheduledGameEvent(event, tickNumber + numTicks))
+    fun queueEvent(event: GameEvent) {
+        eventQueue.add(event)
     }
 
     private fun dequeueEventsToTrigger(): Collection<GameEvent> {
         val eventsToTrigger = mutableSetOf<GameEvent>()
-        while (!eventQueue.isEmpty() && eventQueue.peek().tickNumber < tickNumber)
-            eventsToTrigger.add(eventQueue.poll().gameEvent)
+        while (!eventQueue.isEmpty() && eventQueue.peek().hasExpired(currTickNumber))
+            eventsToTrigger.add(eventQueue.poll())
         return eventsToTrigger
     }
 
     fun tick() {
-        tickNumber++
+        currTickNumber++
         for (event in dequeueEventsToTrigger())
-            event.eventCallback(level)
+            event.trigger(level)
     }
 }
--- src/main/kotlin/mcsoc/bedwars/gamestate/GameEvents.kt
+++ src/main/kotlin/mcsoc/bedwars/gamestate/GameEvents.kt
@@ -1,8 +1,23 @@
 package mcsoc.bedwars.gamestate
 
 import net.minecraft.server.level.ServerLevel
+import java.util.UUID
 
-sealed interface GameEvent {
-    val eventCallback: (ServerLevel) -> Unit
+sealed class GameEvent(private val tickNumber: Int): Comparable<GameEvent> {
+    // add codec here
+
+    abstract fun trigger(level: ServerLevel)
+
+    fun hasExpired(currTickNumber: Int): Boolean = tickNumber <= currTickNumber
+
+    override fun compareTo(other: GameEvent): Int {
+        return tickNumber.compareTo(other.tickNumber)
+    }
+
+    data class GolemExpiryEvent(val tickNumber: Int, val entityId: UUID): GameEvent(tickNumber) {
+        override fun trigger(level: ServerLevel) {
+            TODO("Not yet implemented")
+        }
+    }
 }
 
