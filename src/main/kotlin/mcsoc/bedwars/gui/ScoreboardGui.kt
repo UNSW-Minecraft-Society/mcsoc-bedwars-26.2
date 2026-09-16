4ef58b559126e089f97d17b6473a79e0d2cb17a9
Quack2Alice <quackquackalice.120@gmail.com>
2026-09-16 11:01:49 +1000

    Add Lines To Scoreboard Display

--- src/main/kotlin/mcsoc/bedwars/gui/ScoreboardGui.kt
+++ src/main/kotlin/mcsoc/bedwars/gui/ScoreboardGui.kt
@@ -1,45 +1,39 @@
 package mcsoc.bedwars.gui
 
-import mcsoc.bedwars.datatrackers.gameState
 import net.minecraft.network.chat.Component
-import net.minecraft.network.chat.numbers.NumberFormat
 import net.minecraft.server.level.ServerLevel
-import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.scores.DisplaySlot
 import net.minecraft.world.scores.Objective
+import net.minecraft.world.scores.ScoreHolder
 import net.minecraft.world.scores.Scoreboard
 import net.minecraft.world.scores.criteria.ObjectiveCriteria
 
 object ScoreboardGui {
-    private val objectives: MutableList<Objective> = mutableListOf()
+    private val TITLE = "Vedwars"
 
     fun initialiseScoreboard(level: ServerLevel) {
         val scoreboard = level.scoreboard
 
-        objectives += scoreboard.addDummyObjective("Testig")
-        objectives += scoreboard.addDummyObjective("Testig2")
-
-        display(level)
+        // Set title
+        scoreboard.setDisplayObjective(DisplaySlot.SIDEBAR, getOrCreateDummyObjective(scoreboard))
+        // Add lines
+        addLines(scoreboard, listOf("AAAA", "BBBB"))
     }
 
-    fun Scoreboard.addDummyObjective(name: String, displayName: Component) = this.addObjective(
-        name, ObjectiveCriteria.DUMMY, Component.literal(name),
-        ObjectiveCriteria.RenderType.INTEGER, true, null
-    )
+    fun getOrCreateDummyObjective(scoreboard: Scoreboard) : Objective {
+        return scoreboard.getObjective(TITLE) ?: scoreboard.addObjective(TITLE,
+            ObjectiveCriteria.DUMMY, Component.literal(TITLE),
+            ObjectiveCriteria.RenderType.INTEGER, true, null)
+    }
+
+    private fun addLines(scoreboard: Scoreboard, lines: List<String>) {
+        for ((index, line) in lines.withIndex()) {
+            scoreboard.getOrCreatePlayerScore(ScoreHolder.forNameOnly(line), getOrCreateDummyObjective(scoreboard)).set(index)
+        }
+    }
 
     fun clearScoreboard(level: ServerLevel) {
         val scoreboard = level.scoreboard
-        for (objective in objectives)
-            scoreboard.removeObjective(objective)
-        objectives.clear()
-    }
-
-    fun Scoreboard.addDummyObjective(name: String) = this.addDummyObjective(name, Component.literal(name))
-
-    fun display(level: ServerLevel) {
-        val scoreboard = level.scoreboard
-        for (objective in objectives) {
-            scoreboard.setDisplayObjective(DisplaySlot.SIDEBAR, objective)
-        }
+        scoreboard.removeObjective(getOrCreateDummyObjective(scoreboard))
     }
 }
