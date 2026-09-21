1f27d6949e9d54654d64fab3327052f492e4ac25
Quack2Alice <quackquackalice.120@gmail.com>
2026-09-21 22:53:37 +1000

    Add Team Colors, Modify Chat Colors To Be More Distinct.

--- src/main/kotlin/mcsoc/bedwars/datatrackers/ModDataTracker.kt
+++ src/main/kotlin/mcsoc/bedwars/datatrackers/ModDataTracker.kt
@@ -35,6 +35,7 @@ import kotlin.uuid.Uuid
 import kotlin.uuid.toJavaUuid
 import kotlin.uuid.toKotlinUuid
 import net.minecraft.world.scores.Scoreboard
+import net.minecraft.world.scores.TeamColor
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
@@ -414,7 +415,8 @@ private class ModDataStore() : SavedData(), PlayerStateHolder, TeamStateHolder, 
         for (team in scoreboard.playerTeams) scoreboard.removePlayerTeam(team)
         teams.forEach { 
             teams_map[it] = TeamDataRecord()
-            scoreboard.addPlayerTeam(it.getName())
+            val scoreboardTeam = scoreboard.addPlayerTeam(it.getName())
+            scoreboardTeam.color = Optional.of(it.teamColour)
         }
         
         for (scoreboardTeam in scoreboard.playerTeams) scoreboardTeam.isAllowFriendlyFire = false
--- src/main/kotlin/mcsoc/bedwars/utils/Team.kt
+++ src/main/kotlin/mcsoc/bedwars/utils/Team.kt
@@ -6,28 +6,29 @@ import net.minecraft.resources.ResourceKey
 import net.minecraft.world.item.DyeColor
 import net.minecraft.world.item.equipment.trim.TrimMaterial
 import net.minecraft.world.item.equipment.trim.TrimMaterials
+import net.minecraft.world.scores.TeamColor
 
-enum class Team(val chatColour: ChatFormatting, val trimMaterial: ResourceKey<TrimMaterial>, val dyeColour: DyeColor) {
-    RED(ChatFormatting.RED, TrimMaterials.REDSTONE, DyeColor.RED),
-    GREEN(ChatFormatting.GREEN, TrimMaterials.EMERALD, DyeColor.GREEN),
-    BLUE(ChatFormatting.BLUE, TrimMaterials.LAPIS, DyeColor.BLUE),
-    YELLOW(ChatFormatting.YELLOW, TrimMaterials.GOLD, DyeColor.YELLOW),
+enum class Team(val chatColour: ChatFormatting, val trimMaterial: ResourceKey<TrimMaterial>, val dyeColour: DyeColor, val teamColour: TeamColor) {
+    RED(ChatFormatting.RED, TrimMaterials.REDSTONE, DyeColor.RED, TeamColor.RED),
+    GREEN(ChatFormatting.DARK_GREEN, TrimMaterials.EMERALD, DyeColor.GREEN, TeamColor.DARK_GREEN),
+    BLUE(ChatFormatting.DARK_BLUE, TrimMaterials.LAPIS, DyeColor.BLUE, TeamColor.DARK_BLUE),
+    YELLOW(ChatFormatting.YELLOW, TrimMaterials.GOLD, DyeColor.YELLOW, TeamColor.YELLOW),
 
-    CYAN(ChatFormatting.AQUA, TrimMaterials.DIAMOND, DyeColor.CYAN),
-    MAGENTA(ChatFormatting.LIGHT_PURPLE, TrimMaterials.AMETHYST, DyeColor.MAGENTA),
-    ORANGE(ChatFormatting.GOLD, TrimMaterials.RESIN, DyeColor.ORANGE),
-    PURPLE(ChatFormatting.DARK_PURPLE, TrimMaterials.AMETHYST, DyeColor.PURPLE),
+    CYAN(ChatFormatting.AQUA, TrimMaterials.DIAMOND, DyeColor.CYAN, TeamColor.AQUA),
+    MAGENTA(ChatFormatting.LIGHT_PURPLE, TrimMaterials.AMETHYST, DyeColor.MAGENTA, TeamColor.LIGHT_PURPLE),
+    ORANGE(ChatFormatting.GOLD, TrimMaterials.RESIN, DyeColor.ORANGE, TeamColor.GOLD),
+    PURPLE(ChatFormatting.DARK_PURPLE, TrimMaterials.AMETHYST, DyeColor.PURPLE, TeamColor.DARK_PURPLE),
 
-    PINK(ChatFormatting.LIGHT_PURPLE, TrimMaterials.AMETHYST, DyeColor.PINK),
-    BROWN(ChatFormatting.DARK_RED, TrimMaterials.RESIN, DyeColor.BROWN),
-    LIGHT_GREEN(ChatFormatting.GREEN, TrimMaterials.EMERALD, DyeColor.LIME),
-    LIGHT_BLUE(ChatFormatting.BLUE, TrimMaterials.DIAMOND, DyeColor.LIGHT_BLUE),
-    GRAY(ChatFormatting.DARK_GRAY, TrimMaterials.IRON, DyeColor.GRAY),
-    LIGHT_GRAY(ChatFormatting.GRAY, TrimMaterials.IRON, DyeColor.LIGHT_GRAY),
-    WHITE(ChatFormatting.WHITE, TrimMaterials.QUARTZ, DyeColor.WHITE),
-    BLACK(ChatFormatting.BLACK, TrimMaterials.NETHERITE, DyeColor.BLACK),
+    PINK(ChatFormatting.LIGHT_PURPLE, TrimMaterials.AMETHYST, DyeColor.PINK, TeamColor.LIGHT_PURPLE),
+    BROWN(ChatFormatting.DARK_RED, TrimMaterials.RESIN, DyeColor.BROWN, TeamColor.DARK_RED),
+    LIGHT_GREEN(ChatFormatting.GREEN, TrimMaterials.EMERALD, DyeColor.LIME, TeamColor.GREEN),
+    LIGHT_BLUE(ChatFormatting.BLUE, TrimMaterials.DIAMOND, DyeColor.LIGHT_BLUE, TeamColor.BLUE),
+    GRAY(ChatFormatting.DARK_GRAY, TrimMaterials.IRON, DyeColor.GRAY, TeamColor.DARK_GRAY),
+    LIGHT_GRAY(ChatFormatting.GRAY, TrimMaterials.IRON, DyeColor.LIGHT_GRAY, TeamColor.GRAY),
+    WHITE(ChatFormatting.WHITE, TrimMaterials.QUARTZ, DyeColor.WHITE, TeamColor.WHITE),
+    BLACK(ChatFormatting.BLACK, TrimMaterials.NETHERITE, DyeColor.BLACK, TeamColor.BLACK),
 
-    NONE(ChatFormatting.GRAY, TrimMaterials.NETHERITE, DyeColor.WHITE);
+    NONE(ChatFormatting.GRAY, TrimMaterials.NETHERITE, DyeColor.WHITE, TeamColor.GRAY);
 
     fun getName() = name.lowercase()
 
