b3121ed4b8de6e49ea664675148012f54e9030c9
Quack2Alice <quackquackalice.120@gmail.com>
2026-09-22 10:22:36 +1000

    Change Leaping And Swiftness Potions To Strong Variants

--- src/main/kotlin/mcsoc/bedwars/gui/ShopGui.kt
+++ src/main/kotlin/mcsoc/bedwars/gui/ShopGui.kt
@@ -83,8 +83,8 @@ object ShopGui {
 
                 ShopItem(Items.ARROW, 8, Items.GOLD_INGOT, 2),
                 ShopItem(Items.GOLDEN_APPLE, 1, Items.GOLD_INGOT, 3),
-                ShopCustomItem({BedwarsItems.potionItemStack(Potions.LEAPING)}, Items.EMERALD, 1),
-                ShopCustomItem({BedwarsItems.potionItemStack(Potions.SWIFTNESS)}, Items.EMERALD, 1),
+                ShopCustomItem({BedwarsItems.potionItemStack(Potions.STRONG_LEAPING)}, Items.EMERALD, 1),
+                ShopCustomItem({BedwarsItems.potionItemStack(Potions.STRONG_SWIFTNESS)}, Items.EMERALD, 1),
                 ShopCustomItem({BedwarsItems.potionItemStack(Potions.INVISIBILITY)}, Items.EMERALD, 2),
 
                 ShopTeamItem(Team.entries.associateWith { Items.WOOL.pick(it.dyeColour) },
