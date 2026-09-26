854287130da2d378d6f3b7a3a746c751d1052008
Ian <iantiancen@gmail.com>
2026-09-26 21:30:52 +1000

    Clear inventories before clearing active players

--- src/main/kotlin/mcsoc/bedwars/gamestate/GameManager.kt
+++ src/main/kotlin/mcsoc/bedwars/gamestate/GameManager.kt
@@ -170,15 +170,16 @@ class GameManager {
 
             val customEntityData = level.customEntityData
             level.eventQueue.reset()
+
+            for (player in gameState.getActivePlayers().mapNotNull(level.server.playerList::getPlayer)) {
+                player.inventory.clearContent()
+                player.setGameMode(GameType.SPECTATOR)
+            }
             gameState.clearActivePlayers()
             // clear teams - todo
 
             gameState.setGamePhase(GamePhase.INACTIVE)
             gameState.setGamePeriod(GamePeriod.INACTIVE)
-            for (player in gameState.getActivePlayers().mapNotNull(level.server.playerList::getPlayer)) {
-                player.inventory.clearContent()
-                player.setGameMode(GameType.SPECTATOR)
-            }
 
             // Clears Active players, all teams data and player data
             gameState.resetModData()
